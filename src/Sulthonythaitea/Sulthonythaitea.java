ae0a8574d8611a467ef38a1750533826ca4171af
fahmisulthoni <fahmisulthoni30@gmail.com>
2021-10-27 11:54:51 +0700

    thai tea

---
+++ src/Sulthonythaitea/Sulthonythaitea.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sulthonythaitea;
+
+/**
+ *
+ * @author Fahmi Sulthoni 17111042
+ */
+public class Sulthonythaitea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
